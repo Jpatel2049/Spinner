2478fa5fce023ae853a194340c26f4e54cb3223b
Jay Patel <jpatel2049@gmail.com>
2019-10-08 14:17:24 -0400

    Updated Spinner app with new activity view.

---
+++ app/src/main/java/edu/temple/spinner/CanvasActivity.java
@@ -0,0 +1,19 @@
+package edu.temple.spinner;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent rec_int = getIntent();
+        String color = rec_int.getStringExtra("Color");
+        findViewById(R.id.CanvasActivity).setBackgroundColor(Color.parseColor(color));
+    }
+}
